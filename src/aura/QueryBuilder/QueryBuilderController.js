4e5f026b265b6329c81f5499fecfad537eb26bf9
Yaroslav Dehtiar <yaroslav.dehtiar@ctdev.io>
2019-08-28 19:35:33 +0300

    add button, displayList(no)

--- src/aura/QueryBuilder/QueryBuilderController.js
+++ src/aura/QueryBuilder/QueryBuilderController.js
@@ -24,11 +24,29 @@
             });
             component.set('v.fieldList', options);
         });
+        component.set("v.mainObject", event.getParam("value"));
         $A.enqueueAction(action);
     },
 
-    choiceFields: function (cmp, event) {
+    choiceFields: function (component, event) {
         event.getParam("value");
-    }
+        component.set("v.newFields", event.getParam("value"));
+    },
 
+    executeQuery: function (component, event) {
+        // var options = [];
+        var action = component.get("c.finalExecute");
+        action.setParams({objectName: component.get("v.mainObject"), fieldList: component.get("v.newFields")});
+        action.setCallback(this, function (resp) {
+            var arr = resp.getReturnValue();
+            // arr.forEach(function (element) {
+            //     options.push(element);
+            // });
+            console.log(arr[0]);
+            // console.log(options);
+            component.set("v.fieldForExecute", arr);
+            console.log(component.get("v.fieldForExecute"));
+        });
+        $A.enqueueAction(action);
+    }
 });
