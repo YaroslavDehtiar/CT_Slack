88bf95a83aae01eab1eabcb9abdf5495541050cc
Yaroslav Dehtiar <yaroslav.dehtiar@ctdev.io>
2019-08-28 12:15:17 +0300

    working picklist with Objects

--- src/aura/QueryBuilder/QueryBuilderController.js
+++ src/aura/QueryBuilder/QueryBuilderController.js
@@ -1,12 +1,27 @@
 ({
 
-    doInit: function (component, event) {
+    doInit : function (component, event) {
+        var options = [];
         var action = component.get('c.getObjects');
         action.setCallback(this, function (resp) {
-            component.set('v.getObjects', resp.getReturnValue());
-            console.log(resp.getReturnValue());
+            var arr = resp.getReturnValue();
+            arr.forEach(function (el) {
+                options.push({value:el, label:el})
+            });
+            component.set('v.objects', options);
         });
-        // event.getParam("value");
+        $A.enqueueAction(action);
+    },
+
+    handleOptionSelected: function (cmp, event) {
+        var action = cmp.get("c.getFields");
+        action.setParams({str: event.getParam("value")});
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                cmp.set("v.picklist", response.getReturnValue());
+            }
+        });
         $A.enqueueAction(action);
     },
 
