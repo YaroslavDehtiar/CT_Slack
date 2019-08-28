118aff127b17f342d644b9be97c669eae8b41561
Yaroslav Dehtiar <yaroslav.dehtiar@ctdev.io>
2019-08-28 13:29:04 +0300

    add dualbox

--- src/aura/QueryBuilder/QueryBuilderController.js
+++ src/aura/QueryBuilder/QueryBuilderController.js
@@ -13,14 +13,16 @@
         $A.enqueueAction(action);
     },
 
-    handleOptionSelected: function (cmp, event) {
-        var action = cmp.get("c.getFields");
-        action.setParams({str: event.getParam("value")});
-        action.setCallback(this, function (response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                cmp.set("v.picklist", response.getReturnValue());
-            }
+    handleOptionSelected: function (component, event) {
+        var options = [];
+        var action = component.get("c.getFields");
+        action.setParams({objectName: event.getParam("value")});
+        action.setCallback(this, function (resp) {
+            var arr = resp.getReturnValue();
+            arr.forEach(function (el) {
+                options.push({value:el, label:el})
+            });
+            component.set('v.fieldList', options);
         });
         $A.enqueueAction(action);
     },
