9c52f8e206de279a8238bb20ea8037aa7f0baed4
Yaroslav Dehtiar <yaroslav.dehtiar@ctdev.io>
2019-10-30 17:10:33 +0200

    hey!!! i did it!

--- src/lwc/filters/filters.js
+++ src/lwc/filters/filters.js
@@ -64,7 +64,7 @@ export default class Filters extends LightningElement {
         }));
     }
 
-    //todo tododooooo
+    //todo
 //
 //     inpPickedFieldsForFilter: function (component, event) {
 //         const values = event.getSource().get("v.value");
